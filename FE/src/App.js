402722823542fcc4f7d0b317a6deefd614d76c7f
Mark Genson Celis <markgncelis@gmail.com>
2024-02-01 08:12:33 +0100

    commit

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,11 +1,6 @@
 import { CssBaseline, Stack } from '@mui/material';
-// import axios from 'axios'
-import Main from './components/Main/Main';
 import MainForm from './components/MainForm/MainForm';
-import { createContext, useContext, useEffect, useState } from 'react';
-import SlotsDisplay from './components/SlotsDisplay/SlotsDisplay';
 import MapsDisplay from './components/MapsDisplay/MapsDisplay';
-import { ParkingContext } from './context/ParkingContext';
 
 function App() {
   
